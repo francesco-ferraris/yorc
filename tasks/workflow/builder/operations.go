abf90035c4cb710f423a1145c4950a81a5b5f8dc
francesco-ferraris <francesco.ferraris95@gmail.com>
2020-01-21 17:35:12 +0100

    To delete

--- tasks/workflow/builder/operations.go
+++ tasks/workflow/builder/operations.go
@@ -21,5 +21,5 @@ import (
 )
 
 func isAsyncOperation(operation string) bool {
-	return operation == strings.ToLower(tosca.RunnableRunOperationName)
+	return operation == strings.ToLower(tosca.RunnableRunOperationName) || true
 }
